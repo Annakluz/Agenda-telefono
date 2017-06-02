08cc5248c719a896fd8a3a29e42aff4223a3b0de
Anna karina Luz <anna.luz.duran@gmail.com>
2017-06-02 17:40:58 -0500

    tratando jquery

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,8 +1,32 @@
+let contador = 0;
+
 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
     
+      });
+
+
+    
+   /* let $tar =$("<div/>", { "class": "contactoAgregado" });
+    let $nom =$("<h5/>", { "class":"nombreContacto" });
+    let $tel =$("<p/>", {"class":"telefono"});
+    let $contactos =$("#contactos");
+    let $botonagregar =$("#add");
+    
+    $nom.text($("#icon_prefix").val());
+    $tel.text($("#icon_telephone").val());
+    
+    $tar.append($nom);
+    $tar.append($tel);
+    
+    $contactos.append($tar);
+    
+    contador ++;
+    
+    $("span").text(contador);
+    
+    
     //AGREGAR NOMBRE :
     
-    
-  });
+    */
