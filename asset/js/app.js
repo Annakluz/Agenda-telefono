2c8f127ee2eb6e78204a117a243afb2e562b9286
Annakluz <anna.luz.duran@gmail.com>
2017-06-04 01:24:52 -0500

    jquery listo

--- asset/js/app.js
+++ asset/js/app.js
@@ -3,8 +3,27 @@ let contador = 0;
 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+    $("#add").click(agregando);
     
       });
+
+var agregando = function(){
+    let $tar =$("<div/>", { "class": "contactoAgregado" });
+    let $nom =$("<h5/>", { "class":"nombreContacto" });
+    let $tel =$("<p/>", {"class":"telefono"});
+    let $contactos =$("#contactos");
+     $nom.text($("#icon_prefix").val());
+    $tel.text($("#icon_telephone").val());
+    
+    $tar.append($nom);
+    $tar.append($tel);
+    
+    $contactos.append($tar);
+    
+    contador ++;
+    
+    $("span").text(contador);
+}
 
 
     
